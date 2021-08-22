7914c86bedaf6c8724869e9cf50c7ffef8c10570
mk <pg.studykm@gmail.com>
2021-08-22 13:59:15 +0000

    top page

---
+++ resources/views/top.blade.php
@@ -0,0 +1,162 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <header>
+	<h1>旅シェア</h1>
+	<nav>
+    <ul class="menu">
+	<a href="/"><li>ユーザー登録</li></a>
+	<a href="/"><li>ログイン</li></a>
+	<a href="/"><li>お問い合わせ</li></a>
+	</ul>
+　　</nav>
+	</header>
+    <link rel="stylesheet" href="{{  asset('css/style.css') }}" />
+</head>
+
+
+
+<body>
+  
+<h2>みんなの旅一覧</h2>
+
+<div class="grid">
+  
+  
+  <div class="l-wrapper_01">
+  <article class="card_01">
+    <div class="card__header_01">
+      <p class="card__title_01">タイトル</p>
+      <figure class="card__thumbnail_01">
+        <img src="css/text_ryokou.png" alt="サムネイル" class="card__image_01">
+    </figure>
+    </div>
+    <div class="card__footer_01">
+      <p class="card__text_01"><a href="#" class="button_01 -compact">この旅を詳しく見る!</a></p>
+    </div>
+   </article>
+  </div>
+  
+ <div class="l-wrapper_01">
+  <article class="card_01">
+    <div class="card__header_01">
+      <p class="card__title_01">タイトル</p>
+      <figure class="card__thumbnail_01">
+        <img src="css/text_ryokou.png" alt="サムネイル" class="card__image_01">
+    </figure>
+    </div>
+    <div class="card__footer_01">
+      <p class="card__text_01"><a href="#" class="button_01 -compact">この旅を詳しく見る!</a></p>
+    </div>
+  </article>
+ </div>
+  
+  <div class="l-wrapper_01">
+  <article class="card_01">
+    <div class="card__header_01">
+      <p class="card__title_01">タイトル</p>
+      <figure class="card__thumbnail_01">
+        <img src="css/text_ryokou.png" alt="サムネイル" class="card__image_01">
+    </figure>
+    </div>
+    <div class="card__footer_01">
+      <p class="card__text_01"><a href="#" class="button_01 -compact">この旅を詳しく見る!</a></p>
+    </div>
+  </article>
+</div>
+  
+  <div class="l-wrapper_01">
+  <article class="card_01">
+    <div class="card__header_01">
+      <p class="card__title_01">タイトル</p>
+      <figure class="card__thumbnail_01">
+        <img src="css/text_ryokou.png" alt="サムネイル" class="card__image_01">
+    </figure>
+    </div>
+    <div class="card__footer_01">
+      <p class="card__text_01"><a href="#" class="button_01 -compact">この旅を詳しく見る!</a></p>
+    </div>
+  </article>
+</div>
+  
+</div>
+
+
+
+
+
+<div class="grid">
+  
+  
+  <div class="l-wrapper_01">
+  <article class="card_01">
+    <div class="card__header_01">
+      <p class="card__title_01">タイトル</p>
+      <figure class="card__thumbnail_01">
+        <img src="css/text_ryokou.png" alt="サムネイル" class="card__image_01">
+    </figure>
+    </div>
+    <div class="card__footer_01">
+      <p class="card__text_01"><a href="#" class="button_01 -compact">この旅を詳しく見る!</a></p>
+    </div>
+   </article>
+  </div>
+  
+ <div class="l-wrapper_01">
+  <article class="card_01">
+    <div class="card__header_01">
+      <p class="card__title_01">タイトル</p>
+      <figure class="card__thumbnail_01">
+        <img src="css/text_ryokou.png" alt="サムネイル" class="card__image_01">
+    </figure>
+    </div>
+    <div class="card__footer_01">
+      <p class="card__text_01"><a href="#" class="button_01 -compact">この旅を詳しく見る!</a></p>
+    </div>
+  </article>
+ </div>
+  
+  <div class="l-wrapper_01">
+  <article class="card_01">
+    <div class="card__header_01">
+      <p class="card__title_01">タイトル</p>
+      <figure class="card__thumbnail_01">
+        <img src="css/text_ryokou.png" alt="サムネイル" class="card__image_01">
+    </figure>
+    </div>
+    <div class="card__footer_01">
+      <p class="card__text_01"><a href="#" class="button_01 -compact">この旅を詳しく見る!</a></p>
+    </div>
+  </article>
+　</div>
+  
+  <div class="l-wrapper_01">
+  <article class="card_01">
+    <div class="card__header_01">
+      <p class="card__title_01">タイトル</p>
+      <figure class="card__thumbnail_01">
+        <img src="css/text_ryokou.png" alt="サムネイル" class="card__image_01">
+    </figure>
+    </div>
+    <div class="card__footer_01">
+      <p class="card__text_01"><a href="#" class="button_01 -compact">この旅を詳しく見る!</a></p>
+    </div>
+  </article>
+　　</div>
+  
+</div>
+
+<ul class="example3">
+<li class="pre">前へ</li>
+<li class="this">1</li>
+<li><a href="page-2.html">2</a></li>
+<li><a href="page-3.html">3</a></li>
+<li><a href="page-2.html">次へ</a></li>
+</ul>
+
+</body>
+
+@include("parts.footer")
+
+
+</html>
